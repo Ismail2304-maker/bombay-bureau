3ba229950639936119eabfa83900943bd45a2567
Muhammed Ismail <ismailmuhammed515@gmail.com>
2026-02-14 23:03:39 +0530

    Move sanity config into sanity folder

--- sanity/sanity.config.ts
+++ sanity/sanity.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "sanity"
 import { structureTool } from "sanity/structure"
 import { visionTool } from "@sanity/vision"
-import { schemaTypes } from "./sanity/schemaTypes"
+import { schemaTypes } from "./schemaTypes"
 
 export default defineConfig({
   name: "default",
@@ -13,6 +13,15 @@ export default defineConfig({
   plugins: [structureTool(), visionTool()],
 
   schema: {
-    types: schemaTypes,
-  },
+  types: [
+    {
+      name: "testDoc",
+      title: "Test Doc",
+      type: "document",
+      fields: [
+        { name: "title", type: "string" }
+      ]
+    }
+  ],
+},
 })
