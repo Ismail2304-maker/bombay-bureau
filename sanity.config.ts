3f6e6ea99f2845c877b7543e737898221619f429
Muhammed Ismail <ismailmuhammed515@gmail.com>
2026-02-14 22:56:45 +0530

    Fix sanity config direct env binding

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,16 +1,14 @@
 import { defineConfig } from "sanity"
 import { structureTool } from "sanity/structure"
 import { visionTool } from "@sanity/vision"
-
-import { projectId, dataset } from "./sanity/env"
 import { schemaTypes } from "./sanity/schemaTypes"
 
 export default defineConfig({
   name: "default",
   title: "Bombay Bureau",
 
-  projectId,
-  dataset,
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
 
   plugins: [structureTool(), visionTool()],
 
