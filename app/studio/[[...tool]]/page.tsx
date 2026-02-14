619ee4fb4b4e0ffbc123ad4915b5dc62288178d3
Muhammed Ismail <ismailmuhammed515@gmail.com>
2026-02-14 20:07:13 +0530

    final studio fix

--- app/studio/[[...tool]]/page.tsx
+++ app/studio/[[...tool]]/page.tsx
@@ -1,8 +1,10 @@
-import { NextStudio } from "next-sanity/studio"
-import config from "@/sanity.config"
+import { NextStudio } from "next-sanity/studio";
+import config from "@/sanity.config";
 
-export const dynamic = "force-dynamic"
+export const dynamic = "force-dynamic";
+export const fetchCache = "force-no-store";
+export const revalidate = 0;
 
 export default function StudioPage() {
-  return <NextStudio config={config} />
+  return <NextStudio config={config} />;
 }
