92427514dfba41ac3feaecf8fb78ff008f9b4846
Muhammed Ismail <ismailmuhammed515@gmail.com>
2026-02-15 11:07:00 +0530

    Disable Next image optimization

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,9 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
   images: {
-    domains: ['cdn.sanity.io'],
+    unoptimized: true,
   },
 };
 
-export default nextConfig; 
+export default nextConfig;
