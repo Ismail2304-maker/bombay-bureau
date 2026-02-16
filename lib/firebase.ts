452393940d456fa8462eac592f566f44791b140d
Muhammed Ismail <ismailmuhammed515@gmail.com>
2026-02-16 13:56:41 +0530

    Fix Firebase config - correct API key

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -3,15 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBvFKyFVlay-tmXc4o9AbdZxeA6JPx9DXU",
+  apiKey: "AIzaSyDkfTwHN4HZovG3CxyF87tYaWKnrql81Mg",
   authDomain: "bombay-bureau.firebaseapp.com",
   projectId: "bombay-bureau",
   storageBucket: "bombay-bureau.firebasestorage.app",
   messagingSenderId: "809339827214",
   appId: "1:809339827214:web:8c621d448da6d90446785f",
-  measurementId: "G-Q0Z5PDFM4Z",
 };
 
 const app = initializeApp(firebaseConfig);
-
 export const auth = getAuth(app);
