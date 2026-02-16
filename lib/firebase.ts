e74fc198a05a2d1341dd8b1741da55baff673d08
Muhammed Ismail <ismailmuhammed515@gmail.com>
2026-02-16 14:55:48 +0530

    Fix Firebase config for Bombay Bureau

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,15 +1,19 @@
-import { initializeApp } from "firebase/app";
+// lib/firebase.ts
+
+import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD7xIR3bHEhlm7TwsqRmpaFgCzTNiaP3-Y",
-  authDomain: "global-chronicle-662c9.firebaseapp.com",
-  projectId: "global-chronicle-662c9",
-  storageBucket: "global-chronicle-662c9.firebasestorage.app",
-  messagingSenderId: "231668259310",
-  appId: "1:231668259310:web:59b9bf633bc846ca3356ff",
-  measurementId: "G-XELWXQMP79"
+  apiKey: "AIzaSyDkfTwHN4HZovG3CxyF87tYaWKnrql81Mg",
+  authDomain: "bombay-bureau.firebaseapp.com",
+  projectId: "bombay-bureau",
+  storageBucket: "bombay-bureau.firebasestorage.app",
+  messagingSenderId: "809339827214",
+  appId: "1:809339827214:web:8c621d448da6d90446785f",
+  measurementId: "G-Q0Z5PDFM4Z",
 };
 
-const app = initializeApp(firebaseConfig);
+// Prevent multiple initialization in Next.js
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+
 export const auth = getAuth(app);
