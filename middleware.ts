641f0ef849788010f373aaef7958a8524a6198f5
Muhammed Ismail <ismailmuhammed515@gmail.com>
2026-04-17 18:07:36 +0530

    disable maintenance mode

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-const MAINTENANCE_MODE = true; // change to false to reopen site
+const MAINTENANCE_MODE = false; // change to false to reopen site
 
 export function middleware(request: NextRequest) {
   if (MAINTENANCE_MODE) {
