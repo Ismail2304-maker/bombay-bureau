0ce11eaacb68fdc9dfd6b8f1c15aeaa304bd095d
Ismail <muhammedismailps232@gmail.com>
2026-09-18 17:38:38 +0530

    Include newsroom contact page in sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -15,6 +15,7 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const staticPaths = [
     "",
     "/about",
+    "/contact",
     "/privacy",
     "/terms",
     "/editorial-standards",
