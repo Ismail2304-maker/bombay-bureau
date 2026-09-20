37eebab16662d4540fee544864875bf28537dde5
Ismail <muhammedismailps232@gmail.com>
2026-09-20 19:18:12 +0530

    Reset category pages to the top on navigation

--- app/[category]/page.tsx
+++ app/[category]/page.tsx
@@ -1,4 +1,5 @@
 import Header from "@/components/Header";
+import CategoryScrollReset from "@/components/CategoryScrollReset";
 import { client } from "@/lib/sanity";
 import imageUrlBuilder from "@sanity/image-url";
 import Image from "next/image";
@@ -50,6 +51,7 @@ export default async function CategoryPage(props:any){
   ]};
 
   return <main className="bg-black text-white min-h-screen">
+    <CategoryScrollReset />
     <script type="application/ld+json" dangerouslySetInnerHTML={{__html:JSON.stringify(breadcrumbJsonLd)}} />
     <Header />
     <section className="max-w-7xl mx-auto px-4 md:px-6 mt-8 md:mt-12">
